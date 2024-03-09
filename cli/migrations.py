9b6eee9966d2ff5487ae3eaae4d9d667e5bd77bd
Gurgen Abramyan <gurgen.abramyan.1995@gmail.com>
2024-03-09 13:20:21 +0300

    Alembic fixes.

--- cli/migrations.py
+++ cli/migrations.py
@@ -1,6 +1,4 @@
-import subprocess
-import sys
-
+import alembic.config
 import click
 
 from utils.constants import BASE_DIR
@@ -18,41 +16,21 @@ def migrations() -> None:
 @click.argument("message", type=str)
 def make(message: str) -> None:
     """Создать миграцию."""
-    try:
-        subprocess.check_call(f'alembic -c {ALEMBIC_CONFIG_FILE} revision --autogenerate -m "{message}"', shell=True)
-    except subprocess.CalledProcessError as exc:
-        sys.exit(exc.returncode)
+    alembic_args = ["-c", str(ALEMBIC_CONFIG_FILE), "revision", "--autogenerate", "--message", message]
+    alembic.config.main(argv=alembic_args)
 
 
 @migrations.command()
 @click.option("--revision", default="head", type=str)
 def up(revision: str) -> None:
     """Обновить до заданной версии."""
-    _validate_revision(revision)
-
-    try:
-        subprocess.check_call(f"alembic -c {ALEMBIC_CONFIG_FILE} upgrade {revision}", shell=True)
-    except subprocess.CalledProcessError as exc:
-        sys.exit(exc.returncode)
+    alembic_args = ["-c", str(ALEMBIC_CONFIG_FILE), "upgrade", revision.strip()]
+    alembic.config.main(argv=alembic_args)
 
 
 @migrations.command()
 @click.option("--revision", default="-1")
 def down(revision: str) -> None:
     """Откатить до заданной версии."""
-    _validate_revision(revision)
-
-    try:
-        subprocess.check_call(f"alembic -c {ALEMBIC_CONFIG_FILE} downgrade {revision}", shell=True)
-    except subprocess.CalledProcessError as exc:
-        sys.exit(exc.returncode)
-
-
-def _validate_revision(revision: str) -> None:
-    """Validate revision name.
-
-    :param revision: Revision name.
-    """
-    message = f"Invalid revision: '{revision}'. Revision must be one word without any spaces."
-    if " " in revision.strip():
-        raise click.BadArgumentUsage(message)
+    alembic_args = ["-c", str(ALEMBIC_CONFIG_FILE), "downgrade", revision.strip()]
+    alembic.config.main(argv=alembic_args)
